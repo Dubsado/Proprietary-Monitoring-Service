f1dd88f9aff50428d56ecbcd1a1b8ef400b66573
Daniel Di Venere <danieldivenere@gmail.com>
2024-07-26 13:47:37 -0700

    add server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 8000,
+  fetch(req) {
+    return new Response("Hello, Bun!", { status: 200 });
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}`);
